73433f75a10ab32d7aa439f5b4ad8f7e789c8a6f
FlavioUser222 <flavinhoth12@gmail.com>
2025-10-16 17:05:54 -0300

    a

--- script.js
+++ script.js
@@ -3,24 +3,30 @@ let tamanhoDaLetra = 100;
 let modoEscuro = false;
 let leitorLigado = false;
 
+
+let modoPronatopia = false
+let modoDeuteratopia = false
+let modoTritanopia = false
+
 let botaoMais = document.getElementById("increaseFont");
 let botaoMenos = document.getElementById("decreaseFont");
 let botaoEscuro = document.getElementById("toggleContrast");
 let botaoLeitor = document.getElementById("toggleVoice");
-let botaoVideos = document.getElementById("botaoVideos");
+
+let botaoDaltonismoProtanopia = document.getElementById('toggleColors1')
+let botaoDaltonismoDeuteranopia = document.getElementById('toggleColors2')
+let botaoDaltonismoTritanopia = document.getElementById('toggleColors3')
+
+let botaoVideos = document.getElementById("botaoVideos")
 
 
 botaoMais.onclick = function () {
-    if(tamanhoDaLetra == 170){
+    if (tamanhoDaLetra == 170) {
         return
     }
     tamanhoDaLetra = tamanhoDaLetra + 10;
     document.body.style.fontSize = tamanhoDaLetra + "%"
-
-
-
 };
-
 
 botaoMenos.onclick = function () {
     if (tamanhoDaLetra > 80) {
@@ -28,6 +34,56 @@ botaoMenos.onclick = function () {
         document.body.style.fontSize = tamanhoDaLetra + "%";
     }
 };
+
+
+function limparModosDaltonismo() {
+    document.body.classList.remove("protanopia", "deuteranopia", "tritanopia");
+    modoPronatopia = false;
+    modoDeuteratopia = false;
+    modoTritanopia = false;
+}
+
+
+function ativarDaltonismoTritanopia() {
+    const ativo = !modoTritanopia;
+    limparModosDaltonismo();
+    modoTritanopia = ativo;
+    if (ativo) document.body.classList.add("tritanopia");
+        alert("Modo daltonismo tritanopia")
+
+}
+
+function ativarDaltonismoDeuteratopia() {
+    const ativo = !modoDeuteratopia;
+    limparModosDaltonismo();
+    modoDeuteratopia = ativo;
+    if (ativo) document.body.classList.add("deuteranopia");
+        alert("Modo daltonismo deuteratopia")
+
+}
+
+
+function ativarDaltonismoProtanopia() {
+    const ativo = !modoPronatopia;
+    limparModosDaltonismo();
+    modoPronatopia = ativo;
+    if (ativo) document.body.classList.add("protanopia");
+    alert("Modo daltonismo protanopia")
+}
+
+document.getElementById('toggleColors1').addEventListener('click', ativarDaltonismoTritanopia)
+document.getElementById('toggleColors2').addEventListener('click', ativarDaltonismoDeuteratopia)
+document.getElementById('toggleColors3').addEventListener('click', ativarDaltonismoProtanopia)
+
+
+
+
+
+
+
+
+
+
 
 
 botaoEscuro.onclick = function () {
